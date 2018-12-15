22150b1ffe3fc781956b95c1bceefcb4b106c861
habnernascimento <contato@psdagenciadigital.com>
2018-12-15 13:49:29 -0300

    exercício 2, aula 1

---
+++ Aula 1 - HTML/exercicio_02/cadastro.php
@@ -0,0 +1,5 @@
+<?php
+
+	//echo $_POST["sexo"];
+	//echo $_GET["nome_completo"];
+	echo $_REQUEST["sexo"];
---
+++ Aula 1 - HTML/exercicio_02/input.php
@@ -0,0 +1,14 @@
+<form action="cadastro.php" method="post">
+
+Nome: <input type="text" name="nome_completo" value=""><br>
+País: 
+	<input type="radio" name="pais" value="BRA">Brasil
+	<input type="radio" name="pais" value="ARG">Argentina
+	<input type="radio" name="pais" value="EUA">EUA
+<br>
+Sexo: 	<input type="radio" name="sexo" value="M">Masculino
+		<input type="radio" name="sexo" value="F">Feminino
+<br>
+<input type="submit" value="Enviar dados">
+
+</form>
